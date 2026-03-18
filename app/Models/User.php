c66854b927608ce284c1fa4ae52492c17d1502ca
Alex Garrett-Smith <alex@alex.gs>
2026-03-18 10:28:57 +0000

    Voting backend

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -53,5 +54,13 @@ class User extends Authenticatable
     public function ideas(): HasMany
     {
         return $this->hasMany(Idea::class);
+    }
+
+    /**
+     * @return BelongsToMany<Idea, $this>
+     */
+    public function votedIdeas(): BelongsToMany
+    {
+        return $this->belongsToMany(Idea::class, 'idea_vote');
     }
 }
