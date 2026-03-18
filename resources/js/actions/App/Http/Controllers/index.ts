4e34c73b046601f655de4688644ebca051d3d545
Alex Garrett-Smith <alex@alex.gs>
2026-03-18 12:26:59 +0000

    Refactor: API resources (#17)
    * Use API resources
    
    * Tweaks

--- resources/js/actions/App/Http/Controllers/index.ts
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -1,8 +1,10 @@
+import HomeController from './HomeController'
 import AccountSettingsController from './AccountSettingsController'
 import IdeaController from './IdeaController'
 import VoteController from './VoteController'
 
 const Controllers = {
+    HomeController: Object.assign(HomeController, HomeController),
     AccountSettingsController: Object.assign(AccountSettingsController, AccountSettingsController),
     IdeaController: Object.assign(IdeaController, IdeaController),
     VoteController: Object.assign(VoteController, VoteController),
