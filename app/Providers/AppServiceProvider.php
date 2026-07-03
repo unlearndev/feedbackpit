44fdf465c155b76156053e9ed28f4352ec913312
Alex Garrett-Smith <alex@alex.gs>
2026-07-03 08:53:08 +0100

    Remove the reactions feature flag and ship reactions unconditionally

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Idea;
-use App\Models\User;
 use App\Observers\IdeaObserver;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Pennant\Feature;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,7 +25,5 @@ class AppServiceProvider extends ServiceProvider
         JsonResource::withoutWrapping();
 
         Idea::observe(IdeaObserver::class);
-
-        Feature::define('reactions', fn (?User $user) => false);
     }
 }
